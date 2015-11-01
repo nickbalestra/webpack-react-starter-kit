e849113c1f2848b682161305d3251f2409d58ec7
Nick Balestra <nick@balestra.ch>
2015-11-01 12:58:06 -0800

    updated to babel 6 relase and added relative dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
 var webpack = require('webpack');
 
+var plugins = [
+  new webpack.optimize.CommonsChunkPlugin('public/common.js'),
+];
+
 module.exports = {
+
   entry: {
     Home: './app/Home.js',
     Profile: './app/Profile.js'
   },
 
   output: {
-    filename: 'pubic/[name].js'
+    filename: 'public/[name].js'
   },
+
+  plugins: plugins,
 
   module: {
     loaders: [
-      {test: /\.js$/, loader: 'babel-loader'}
+      {test: /\.js$/, loader: "babel", query: {presets:['react', 'es2015']}}
     ]
   }
 };
